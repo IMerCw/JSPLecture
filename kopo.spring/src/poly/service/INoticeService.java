4ca0f87bae7300cce3bb28e73de36921b0ceaec9
Rock Kim <kingrock.kor@gmail.com>
2018-09-06 22:44:11 +0900

    kopo spring project

---
+++ kopo.spring/src/poly/service/INoticeService.java
@@ -0,0 +1,10 @@
+package poly.service;
+
+import java.util.List;
+
+import poly.dto.NoticeDTO;
+
+@SuppressWarnings("unused")
+public interface INoticeService {
+
+}
---
+++ kopo.spring/src/poly/service/impl/NoticeService.java
@@ -0,0 +1,20 @@
+package poly.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import poly.dto.NoticeDTO;
+import poly.persistance.mapper.NoticeMapper;
+import poly.service.INoticeService;
+
+@SuppressWarnings("unused")
+@Service("NoticeService")
+public class NoticeService implements INoticeService{
+	
+	@Resource(name="NoticeMapper")
+	private NoticeMapper noticeMapper;
+	
+}
